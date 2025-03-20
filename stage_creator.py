5aa1fe67486cbf4fd31a642f4fab6e84fe165f5c
Skyler Burden <burdensk@mail.gvsu.edu>
2025-03-20 11:28:20 -0400

    shortened length of beams and started creating ramp for first level of stage 1

--- stage_creator.py
+++ stage_creator.py
@@ -3,10 +3,21 @@ from stage import Stage
 def create_stage1():
     stage = Stage()
 
-    for i in range(15):
+    flat_y = 700
+    for i in range(4):
         stage.add_element("beam", (128*i, 700), (128, 32))
+    # Second loop: beams going up the screen for the ramped part.
+    # Start where the flat beams ended.
+    start_x = 8 * 64  
+    start_y = flat_y  
+    # Change these values (delta_x and delta_y) to adjust the steepness of your ramp.
+    delta_x = 64  
+    delta_y = -9  # negative y moves upward in pygame
 
-    stage.add_element("oil_barrel", (25, 635)) # oil barrel on bottom left
+    for i in range(8):  # number of beams on the ramp
+        stage.add_element("beam", (512 + i * delta_x, 700 + i * delta_y), (64,32))
+
+    stage.add_element("oil_barrel", (50, 635)) # oil barrel on bottom left
     
     return stage
 
